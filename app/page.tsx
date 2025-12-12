91135947c3874ff0f5faf0070c80baa79f1c9d2a
Shane-Breen <shanebreen@gmail.com>
2025-12-12 13:39:54 +0000

    Add homepage

---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main style={{ padding: 24 }}>
+      <h1>🔥 Where’s My Lighter?</h1>
+      <p>This lighter has been tapped.</p>
+    </main>
+  );
+}
